af1b0893a27d2e1e8542d5017041e9eb43fa86f7
GUDIPATI GOPAL <GUDIPATI VENKATA SESHA SATYA SURYA GOPAL>
2025-01-12 19:29:58 +0530

    Linear Search

---
+++ LinearSearch.cpp
@@ -0,0 +1,33 @@
+// LinearSearch.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A[10]= {7,8,9,10,11,13,15,17,19,21}, n = 10, key;
+
+	cout << "Enter the key to find the number: ";
+	cin >> key;
+
+	for (int i = 0;i < n;i++) {
+		if (key == A[i]) {
+			cout << "Found the key at the index of: " << i;
+			return 0;
+		}
+	}cout << "not found the key in the array ";
+	return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
