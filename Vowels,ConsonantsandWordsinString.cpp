b7d3100e5ad6a8b6c1ad3ca3c879bff35db7c793
GUDIPATI GOPAL <GUDIPATI VENKATA SESHA SATYA SURYA GOPAL>
2025-01-18 23:11:50 +0530

    String Example Problems

---
+++ Vowels,ConsonantsandWordsinString.cpp
@@ -0,0 +1,41 @@
+// Vowels,ConsonantsandWordsinString.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str = "Hi How Are You";
+	int vowels = 0, consonants = 0, spaces = 0;
+
+	for (int i = 0;str[i] != '\0';i++)
+	{
+		if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' || str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+
+			vowels++;
+		else if (str[i] == ' ')
+
+			spaces++;
+
+		else
+			consonants++;
+	}
+	cout << "Vowels are: " << vowels << endl;
+	cout << "consonants are: " << consonants<< endl;
+	cout << "words are: " << spaces +1 << endl;
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
